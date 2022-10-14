36a49fa41309f3f215f1a6a78143f1d8828d9092
hanticoy <hugo.anticoy.anticoy@gmail.com>
2022-10-13 22:55:24 -0300

    actualiza ejecucion

--- Fizzbuzz.js
+++ Fizzbuzz.js
@@ -1,12 +1,19 @@
+var noimprime = 0;
 for (var i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
         console.log('Fizz');
+        noimprime = 1;
     }
     if (i % 5 == 0) {
         console.log('Buzz');
+        noimprime = 1;
     }
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log('FizzBuzz');
+        noimprime = 1;
     }
-    console.log(i);
+    if (noimprime == 0){
+        console.log(i);
+    }
+    noimprime = 0
 }
